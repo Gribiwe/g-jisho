ecae06737cb80ad531841ac9ef3b924c3896ef9e
Gribiwe <grib.morpech@gmail.com>
2019-02-24 13:31:46 +0200

    new entities: dictionary and kanji

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,22 @@ import { RouterModule } from '@angular/router';
 @NgModule({
     imports: [
         RouterModule.forChild([
+            {
+                path: 'dictionary',
+                loadChildren: './dictionary/dictionary.module#GjishoDictionaryModule'
+            },
+            {
+                path: 'kanji-record',
+                loadChildren: './kanji-record/kanji-record.module#GjishoKanjiRecordModule'
+            },
+            {
+                path: 'dictionary',
+                loadChildren: './dictionary/dictionary.module#GjishoDictionaryModule'
+            },
+            {
+                path: 'kanji-record',
+                loadChildren: './kanji-record/kanji-record.module#GjishoKanjiRecordModule'
+            }
             /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
         ])
     ],
